479736fe0dacb897b7a433aa54d2c68cdddfb7fb
Nate Worley <immythz@gmail.com>
2021-03-24 17:01:37 -0500

    Upadated UV logic

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,20 +1,20 @@
-var currentDate = new Date().toLocaleDateString('en-US')
-var cityFormEl = document.querySelector('#cityForm')
-var cityNameEl = document.querySelector('#cityName')
-var cityListEl = document.querySelector('#cityList')
-var weeklyEl = document.querySelector('#weeklyWeather')
-var dailyCardEl = document.querySelector('.daily')
-var searchBtnEl = document.querySelector('#searchBtn')
-var currentWeatherEl = document.querySelector('#currentWeather')
+const currentDate = new Date().toLocaleDateString('en-US')
+const cityFormEl = document.querySelector('#cityForm')
+const cityNameEl = document.querySelector('#cityName')
+const cityListEl = document.querySelector('#cityList')
+const weeklyEl = document.querySelector('#weeklyWeather')
+const dailyCardEl = document.querySelector('.daily')
+const searchBtnEl = document.querySelector('#searchBtn')
+const currentWeatherEl = document.querySelector('#currentWeather')
 
 //Submit user input functions
 cityFormEl.addEventListener('submit', function(event){
     event.preventDefault();
-    var cityNameVal = cityNameEl.value
+    const cityNameVal = cityNameEl.value
     console.log(cityNameEl.value)
   
     if (cityNameVal === '') {
-        var messageEl = cityFormEl.children[1]
+        const messageEl = cityFormEl.children[1]
         messageEl.classList.remove('d-none')
         messageEl.classList.add('d-block')
     } else {
@@ -27,8 +27,8 @@ cityFormEl.addEventListener('submit', function(event){
 
 
 //Makes a list of past searches below input box
-var printCityList = function(name) {
-    var cityListItem = document.createElement('li')
+const printCityList = function(name) {
+    const cityListItem = document.createElement('li')
     cityListItem.classList.add('list-unstyled', 'list-group-item', 'list-group-item-action', 'h6', 'mt-1')
     cityListItem.textContent = name;
     cityListEl.appendChild(cityListItem)
@@ -41,33 +41,34 @@ var printCityList = function(name) {
 };
 
 //All API functions for retrieving data
-var getWeather = function (cityName) {
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=f05e59dca587993db2e06e2c3a372a11'
+const getWeather = function (cityName) {
+    const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=f05e59dca587993db2e06e2c3a372a11'
     fetch(weatherUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data)
-            var iconCode = data.weather[0].icon
-            var iconUrl = 'https://www.openweathermap.org/img/wn/' + iconCode + '.png'
+            const iconCode = data.weather[0].icon
+            const iconUrl = 'https://www.openweathermap.org/img/wn/' + iconCode + '.png'
 
-            var currentWeatherDiv = document.createElement('div')
+            const currentWeatherDiv = document.createElement('div')
             currentWeatherEl.appendChild(currentWeatherDiv)
             currentWeatherDiv.innerHTML = '<h2>' + data.name + ' ' + currentDate + ' ' + '<img src="' + iconUrl + '" alt="Weather Icons">' + '</h2>' + '<h6>' + '<p>' + "Temp: " + Math.round(data.main.temp) + " ℉" + '</p>' + '<p>' + "Humid: " + data.main.humidity + "%" + '</p>' + '<p>' + "Wind Speed: " + data.wind.speed + " MPH" + '</p>' + '</h6>'
 
-            var uvUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&exclude=current,minutely,hourly,alerts&units=imperial&appid=f05e59dca587993db2e06e2c3a372a11'
+            const uvUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&exclude=current,minutely,hourly,alerts&units=imperial&appid=f05e59dca587993db2e06e2c3a372a11'
 
             fetch(uvUrl)
                 .then(function(response){
                     return response.json();
                 })
+                //Creates an element for the UV info and changes the color based on the argument
                 .then(function (data){
                     console.log(data)
-                    var uv1El = document.createElement('div')
+                    const uv1El = document.createElement('div')
                     currentWeatherDiv.appendChild(uv1El)
                     uv1El.innerHTML = '<h6>' + "UV Index: " + data.daily[0].uvi + '</h6>'
-        
+                    
                     if(data.daily[0].uvi === 0 || data.daily[0].uvi < 3){
                         uv1El.setAttribute("style", "color: green;")
                     } else if (data.daily[0].uvi >= 3 && data.daily[0].uvi <= 6) {
@@ -77,14 +78,14 @@ var getWeather = function (cityName) {
                     }
 
                     //Places info in 5-Day Forecast tiles
-                    var weeklyLabelEl = document.querySelector('#weeklyLabel')
+                    const weeklyLabelEl = document.querySelector('#weeklyLabel')
                     weeklyLabelEl.innerHTML = '<h3>' + '5-Day Forecast:' + '</h3>'
 
                     for(var i = 1; i < 6; i++){
-                        var dailyDate = new Date(data.daily[i].dt * 1000).toLocaleDateString('en-US')
-                        var dailyWeatherIcon = data.daily[i].weather[0].icon
-                        var iconUrl = 'https://www.openweathermap.org/img/wn/' + dailyWeatherIcon + '.png'
-                        var weeklyDiv = document.createElement('div')
+                        const dailyDate = new Date(data.daily[i].dt * 1000).toLocaleDateString('en-US')
+                        const dailyWeatherIcon = data.daily[i].weather[0].icon
+                        const iconUrl = 'https://www.openweathermap.org/img/wn/' + dailyWeatherIcon + '.png'
+                        const weeklyDiv = document.createElement('div')
                         weeklyDiv.classList.add('weekly', 'rounded', 'card', 'p-3', 'mr-3', 'bg-gradient')
                         weeklyEl.appendChild(weeklyDiv)
                         weeklyDiv.innerHTML = '<h6>' + '<p>' + dailyDate + '</p>' + '<p>' +  '<img src="' + iconUrl + '" alt="Weather Icons">' + '</p>' + '<p>' + "Temp: " + Math.round(data.daily[i].temp.max) + " ℉" + '</p>' + '<p>' + "Humid: " + data.daily[i].humidity + "%" + '</p>' + '</h6>'
